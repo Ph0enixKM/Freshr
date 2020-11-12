67b2916bc5709de43be409c8dd40a1950fb839b1
Ph0enixKM <pkaras.it@gmail.com>
2020-11-12 20:50:25 +0100

    Counter

--- popup.js
+++ popup.js
@@ -2,6 +2,7 @@ const button = document.querySelector('button')
 const seconds = document.querySelector('#sec')
 const hint = document.querySelector('.hint')
 let sec = 30
+let remain = 30
 let time = null
 
 update()
@@ -11,23 +12,26 @@ function update() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             let tab = tabs[0]
             chrome.tabs.update(tab.id, {url: tab.url})
+            remain = sec
         })
     }, sec * 1000)
 }
 
+setInterval(() => {
+    hint.innerHTML = `<span>${remain} seconds</span>`
+    remain--;
+}, 1000)
+
 seconds.addEventListener('input', e => {
-    hint.innerHTML = e.target.value
+    hint.innerHTML = `${e.target.value} seconds`
     sec = +e.target.value
+    remain = sec
     update()
 })
 
 
-button.addEventListener('click', e => {
-    refresh = !refresh
-    button.innerHTML = text[+refresh]
-    update()
-})
-
-chrome.storage.local.get(['key'], function(result) {
-    console.log('Value currently is ' + result.key);
-})
+// button.addEventListener('click', e => {
+//     refresh = !refresh
+//     button.innerHTML = text[+refresh]
+//     update()
+// })
